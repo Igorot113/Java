475dc430552240eff3c951b1deec6213817c9391
Igor Aquino <corvo.enstai@gmail.com>
2023-12-16 09:01:00 -0300

    new file:   .vscode/settings.json 	new file:   README.md 	new file:   bin/ContaTerminal.class 	new file:   src/ContaTerminal.java

---
+++ src/ContaTerminal.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Vamos criar uma conta");
+        System.out.println("Primeiro qual o seu nome: ");
+        
+        
+    }
+    
+}
