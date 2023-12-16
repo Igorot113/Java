5c702d2645bdb514dfaf9cf1c43b6d7695b3271d
Igor Aquino <corvo.enstai@gmail.com>
2023-12-16 09:43:39 -0300

    modified:   bin/ContaTerminal.class 	modified:   src/ContaTerminal.java

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -6,7 +6,21 @@ public class ContaTerminal {
 
         System.out.println("Vamos criar uma conta");
         System.out.println("Primeiro qual o seu nome: ");
+        String nome = scan.nextLine();
+        System.out.println("Digite aqui seu CPF: ");
+        String CPF = scan.nextLine();
+        System.out.println("Digite aqui seu email: ");
+        String Email = scan.nextLine();
+
+        System.out.println("Sua conta foi criada!");
+        System.out.println("Segue abaixo os dados da sua conta: ");
+        System.out.println("Nome da conta: " + nome);
+        System.out.println("CPF: " + CPF);
+        System.out.println("Email da conta: " + Email);
         
+
+
+
         
     }
     
